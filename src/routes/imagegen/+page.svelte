8898dbe1e71755a231dcbe1ac8f3db0cf1ace0be
Jacob Nielsen <jnillern@gmail.com>
2022-10-16 16:26:55 +0200

    Fix gallery for mobile

--- src/routes/imagegen/+page.svelte
+++ src/routes/imagegen/+page.svelte
@@ -6,10 +6,10 @@
 	console.log(data.predictions);
 </script>
 
-<main class="grid grid-cols-5 mt-5 gap-4">
+<main class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 mt-5 gap-4">
 	{#each data.predictions.results as prediction}
 		{#if prediction.status === 'succeeded' && prediction.source === 'api'}
-			<div class="card shadow bg-neutral shadow-xl text-neutral-content">
+			<div class="card bg-neutral shadow-xl text-neutral-content">
 				<figure><img src={prediction.output?.at(0)} alt={prediction.input?.prompt} /></figure>
 				<div class="card-body">
 					<p class=" text-lg">{prediction.input?.prompt}</p>
