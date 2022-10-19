7e444664035c49108ef4d6121fc6b65b311f8258
Jacob Nielsen <jnillern@gmail.com>
2022-10-19 18:32:11 +0200

    each card is now a link to prediction page + preview of all images

--- src/routes/imagegen/+page.svelte
+++ src/routes/imagegen/+page.svelte
@@ -4,15 +4,28 @@
 	export let data: PageServerData;
 </script>
 
-<main class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 mt-5 gap-4">
+<!-- <pre>{JSON.stringify(data, null, 2)}</pre> -->
+
+<main
+	class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 mt-5 gap-4"
+>
 	{#each data.predictions.results as prediction}
 		{#if prediction.status === 'succeeded' && prediction.source === 'api'}
-			<div class="card bg-neutral shadow-xl text-neutral-content">
-				<figure><img src={prediction.output?.at(0)} alt={prediction.input?.prompt} /></figure>
+			<a href={`/imagegen/${prediction.id}`} class="card bg-neutral shadow-xl text-neutral-content">
+				{#if prediction.output.length === 1}
+					<img src={prediction.output?.at(0)} alt={prediction.input?.prompt} />
+				{:else}
+					<figure class="flex flex-wrap">
+						{#each prediction.output as output}
+							<!-- content here -->
+							<img src={output} alt={prediction.input?.prompt} class=" w-1/2" />
+						{/each}
+					</figure>
+				{/if}
 				<div class="card-body">
 					<p class=" text-lg">{prediction.input?.prompt}</p>
 				</div>
-			</div>
+			</a>
 		{/if}
 	{/each}
 </main>
