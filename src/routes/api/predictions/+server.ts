8aad4f206b0b9d59e3a0848e141b18945d6dd1fb
Jacob Nielsen <jnillern@gmail.com>
2022-10-19 02:51:14 +0200

    preparation for infinite scroll

---
+++ src/routes/api/predictions/+server.ts
@@ -0,0 +1,16 @@
+import { json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+import { REPLICATE_API_TOKEN } from '$env/static/private';
+import type { Predictions } from '$lib/types/predictions';
+
+export const GET: RequestHandler = async ({ params, url }) => {
+	const response = await fetch('https://api.replicate.com/v1/predictions', {
+		headers: {
+			Authorization: `Token ${REPLICATE_API_TOKEN}`,
+			'Content-Type': 'application/json'
+		}
+	});
+
+	const predictions: Predictions = await response.json();
+	return json({ params, cursor: url.searchParams.get('cursor') });
+};
