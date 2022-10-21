61335ab81c57600f27225e12360a43802cef8907
Jacob Nielsen <jnillern@gmail.com>
2022-10-21 19:38:44 +0200

    move prompt above images + responsive

--- src/routes/imagegen/[id]/+page.svelte
+++ src/routes/imagegen/[id]/+page.svelte
@@ -6,17 +6,17 @@
 </script>
 
 <main class="flex flex-col mt-5 gap-5">
-	<div
-		class="grid grid-cols-1  m-auto gap-4  "
-		class:grid-cols-2={data.prediction.output.length > 1}
-	>
-		{#each data.prediction.output as image}
-			<img src={image} alt={data.prediction.input?.prompt} class="rounded-lg" />
-		{/each}
-	</div>
 	<div class="alert shadow-lg bg-neutral">
 		<div>
 			<span>{data.prediction.input?.prompt}</span>
 		</div>
+	</div>
+	<div
+		class="grid grid-cols-1  m-auto gap-4  "
+		class:lg:grid-cols-2={data.prediction.output.length > 1}
+	>
+		{#each data.prediction.output as image}
+			<img src={image} alt={data.prediction.input?.prompt} class="rounded-lg" />
+		{/each}
 	</div>
 </main>
