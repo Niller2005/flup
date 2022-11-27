526696b666d977f749da63667590fe3f220d617e
Jacob Nielsen <jnillern@gmail.com>
2022-11-27 21:35:35 +0100

    Add infinity scroll + FATLOL

--- src/routes/api/predictions/+server.ts
+++ src/routes/api/predictions/+server.ts
@@ -4,13 +4,16 @@ import { REPLICATE_API_TOKEN } from '$env/static/private';
 import type { Predictions } from '$lib/types/predictions';
 
 export const GET: RequestHandler = async ({ params, url }) => {
-	const response = await fetch('https://api.replicate.com/v1/predictions', {
-		headers: {
-			Authorization: `Token ${REPLICATE_API_TOKEN}`,
-			'Content-Type': 'application/json'
+	const response = await fetch(
+		`https://api.replicate.com/v1/predictions?cursor=${url.searchParams.get('cursor')}`,
+		{
+			headers: {
+				Authorization: `Token ${REPLICATE_API_TOKEN}`,
+				'Content-Type': 'application/json'
+			}
 		}
-	});
+	);
 
 	const predictions: Predictions = await response.json();
-	return json({ params, cursor: url.searchParams.get('cursor') });
+	return json(predictions);
 };
