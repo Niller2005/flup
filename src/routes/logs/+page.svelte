f605e01d4bc1949e0495c538b66a4324a489d9b0
Jacob Nielsen <jnillern@gmail.com>
2023-01-11 21:19:02 +0100

    Add logs page

---
+++ src/routes/logs/+page.svelte
@@ -0,0 +1,4 @@
+<main class="flex flex-col justify-center items-center h-full">
+	<img src="//cdn.7tv.app/emote/60ae2440aee2aa553892c5f5/4x.webp" loading="lazy" alt="" />
+	<span class="text-xl">Logs coming soon</span>
+</main>
