a4d3e80cd914045484ccfb8e04b7246c46b3b58c
Jacob Nielsen <jnillern@gmail.com>
2023-01-11 22:32:12 +0100

    Add drawer menu for mobile

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,11 +1,25 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte/dist/OfflineIcon.svelte';
+	import bars3Solid from '@iconify/icons-heroicons/bars-3-solid';
+	import type { PageLink } from '$lib/types/pagelink';
+
+	export let pages: PageLink[];
+</script>
+
 <div class="navbar bg-neutral text-neutral-content shadow rounded-lg">
 	<div class="flex-1">
 		<a class="btn btn-ghost normal-case text-xl" href="/">Flupbot</a>
 	</div>
-	<div class="flex-none">
+	<div class="flex-none lg:hidden">
+		<label for="nav-drawer" class="btn btn-square btn-ghost">
+			<Icon icon={bars3Solid} class="w-7 h-7" />
+		</label>
+	</div>
+	<div class="flex-none hidden lg:block">
 		<ul class="menu menu-horizontal p-0">
-			<li><a href="/music">Music</a></li>
-			<li><a href="/imagegen">Generated Images</a></li>
+			{#each pages as page}
+				<li><a href={page.href}>{page.title}</a></li>
+			{/each}
 		</ul>
 	</div>
 </div>
