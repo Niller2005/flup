2558b79b180a2747698747ba24a6af89e67d214f
krimsonknite <scorpy_zr@hotmail.com>
2023-09-18 13:36:26 -0700

    test fat 1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,4 +5,5 @@
 		alt=""
 	/>
 	<span class="text-5xl">Alo bois</span>
+	<span class="text-xs">Fatty was here</span>
 </main>
---
+++ src/routes/projectlist/+page.svelte
@@ -0,0 +1,9 @@
+<main class="flex flex-col justify-center items-center h-full">
+	<img
+		src="//static-cdn.jtvnw.net/emoticons/v2/emotesv2_e5a46ab9c8854c3fa42641788b2f5cd8/animated/light/3.0"
+		loading="lazy"
+		alt=""
+	/>
+	<span class="text-xl">Work in progress</span>
+	<span class="text-xl">List here. Checkbox style. Add Item. Saves in Flup-database.</span>
+</main>
